d971f8d223eb8a01f379d1019529291d8cc2d914
APAishwarya <aishwarya.apmanoj@gmail.com>
2023-07-10 15:35:59 +0530

    done

---
+++ project/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+ import "bootstrap/dist/css/bootstrap.min.css"
+ import Home from './Components/Home'
+ import Menu from './Components/Menu'
+ import AboutUs from './Components/AboutUs'
+ import NavBar from './Components/NavBar'
+ import{BrowserRouter, Routes, Route} from "react-router-dom"
+
+function App(){
+  return(
+    <>
+    {/* <h1 classname="bg-primary">React</h1>
+    <p>This is a paragraph</p> */}
+    <BrowserRouter>
+    <NavBar/>
+    <Routes>
+      <Route path ="/" element={<Home/>}/>
+      <Route path ="/about" element={<AboutUs/>}/>
+      <Route path ="/menu" element={<Menu/>}/>
+    </Routes>
+    </BrowserRouter>
+    </>
+  );
+}
+export default App;
---
+++ project/src/Components/Home.js
@@ -0,0 +1,29 @@
+
+
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+<h2>Welcome to Good Food</h2>
+<img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.deputy.com%2Fuploads%2F2018%2F10%2FThe-Most-Popular-Menu-Items-That-You-should-Consider-Adding-to-Your-Restaurant_Content-image1-min-1024x569.png&tbnid=y9Ay68EeHtgn_M&vet=12ahUKEwivmPDD7oOAAxXdnWMGHdrjBfAQMygWegUIARCUAg..i&imgrefurl=https%3A%2F%2Fwww.deputy.com%2Fblog%2Fthe-most-popular-menu-items-that-you-should-consider-adding-to-your-restaurant&docid=m4DbhFWDlxCDlM&w=1024&h=569&q=food%20items&ved=2ahUKEwivmPDD7oOAAxXdnWMGHdrjBfAQMygWegUIARCUAg"
+                    alt="Loading"
+                  
+                    />
+    <center>
+        <>
+        Welcome to Food World!
+
+dnffjg njdskfn sfnsbd sdvfnjbg bnsdbfd        </>
+    </center>
+
+</>
+  
+
+
+ 
+  )
+  
+}
+
+export default Home
